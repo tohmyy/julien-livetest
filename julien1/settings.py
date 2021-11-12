2fa71fd8ee09b48b545eb31f4ebde48c9b7e84a4
tohmyy <71967643+tohmyy@users.noreply.github.com>
2021-11-12 16:45:10 +0100

    Update settings.py

--- julien1/settings.py
+++ julien1/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'julien1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'accounts/templates/accounts')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
