2579a794b155a9c944ec39c53659388cb96f4041
tohmyy <71967643+tohmyy@users.noreply.github.com>
2021-11-12 15:49:41 +0100

    Update settings.py

--- julien1/settings.py
+++ julien1/settings.py
@@ -121,7 +121,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
